5ce040b806d0a18ed9f20f128109395de60c21b7
Marc Walter <marc.walter@bsi-software.com>
2024-06-20 14:29:16 +0200

    Rename ulm-compiler dir

---
+++ ulm-compiler-wasm/Ulm.hs
@@ -0,0 +1,128 @@
+module Ulm where
+
+import AST.Canonical qualified as Can
+import AST.Optimized as Opt
+import AST.Source qualified as Src
+import Compile qualified
+import Data.ByteString.Builder qualified
+import Data.ByteString.Lazy qualified
+import Data.ByteString.Lazy.UTF8 qualified -- from utf8-string
+import Data.ByteString.UTF8 qualified as BSU -- from utf8-string
+import Data.Map qualified as Map
+import Data.Name qualified
+import Debug.Trace
+import Elm.ModuleName qualified as ModuleName
+import Elm.Package qualified as Pkg
+import File qualified
+import GHC.Wasm.Prim qualified as Wasm -- See https://gitlab.haskell.org/ghc/ghc/-/commit/317a915bc46fee2c824d595b0d618057bf7fbbf1#82b5a034883a3ede9540d6423738da627660f860
+import Generate.JavaScript qualified as JS
+import Generate.Mode qualified as Mode
+import Json.Encode ((==>))
+import Json.Encode qualified
+import Parse.Module qualified as Parse
+import Reporting.Error qualified
+import Reporting.Error.Syntax qualified as Syntax
+import Reporting.Exit.Help qualified
+import Ulm.ReadArtifacts qualified as ReadArtifacts
+import Ulm.Reporting qualified
+
+main :: IO ()
+main = mempty
+
+foreign export javascript "buildArtifacts" buildArtifacts :: IO ()
+
+buildArtifacts = putStrLn "TODO buildArtifacts"
+
+-- The main compilation logic is the same as
+-- `../worker/src/Endpoint/Compile.hs``
+
+data Outcome
+  = Success ModuleName.Raw String
+  | NoMain
+  | BadInput ModuleName.Raw Reporting.Error.Error
+
+foreign export javascript "compile"
+  compileWasm :: Wasm.JSString -> IO Wasm.JSString
+
+compileWasm :: Wasm.JSString -> IO Wasm.JSString
+compileWasm jsString =
+  let str = Wasm.fromJSString jsString
+      source = BSU.fromString $ trace "parsing" $ traceShowId str
+   in do
+        trace "wrote sample file" $ Data.ByteString.Builder.writeFile "/wasm-can-write" (Data.ByteString.Builder.stringUtf8 "horst")
+        trace "wrote sample file" $ Data.ByteString.Builder.writeFile "/packages/wasm-can-write" (Data.ByteString.Builder.stringUtf8 "horst")
+        outcome <- compile source
+        pure $ encodeJson $ outcomeToJson source outcome
+
+compile :: BSU.ByteString -> IO Outcome
+compile source =
+  case parse source of
+    Left err ->
+      pure $ BadInput Data.Name._Main (Reporting.Error.BadSyntax err)
+    Right modul@(Src.Module _ _ _ imports _ _ _ _ _) ->
+      -- TODO add a way to verify imports
+      let importNames = fmap Src.getImportName imports
+       in do
+            artifacts <- ReadArtifacts.getArtifactsForWasm
+            -- traceShow "show importNames" $ traceShow importNames $
+            case Compile.compile Pkg.dummyName (ReadArtifacts.interfaces artifacts) modul of
+              Left err ->
+                pure $ BadInput (Src.getName modul) err
+              Right (Compile.Artifacts canModule _ locals) ->
+                trace "Compile did not fail" $ case locals of
+                  Opt.LocalGraph Nothing _ _ ->
+                    pure NoMain
+                  Opt.LocalGraph (Just main_) _ _ ->
+                    let mode = Mode.Dev Nothing
+                        home = Can._name canModule
+                        name = ModuleName._module home
+                        mains = Map.singleton home main_
+                        graph = Opt.addLocalGraph locals (ReadArtifacts.objects artifacts)
+                        js :: Data.ByteString.Builder.Builder
+                        js = trace "generated js" $ JS.generate mode graph mains
+                        _ = traceShowId js
+                        filename = "generated.js"
+                        filepath = "/tmp/" ++ filename
+                     in do
+                          trace "Success3, generated JS code" $ Data.ByteString.Builder.writeFile "/absolute.js" js
+                          trace "Success2, generated JS code" $ Data.ByteString.Builder.writeFile "./relative.js" js
+                          trace "Success, generated JS code" $ Data.ByteString.Builder.writeFile filepath js
+                          pure $ Success name filepath
+
+parse :: BSU.ByteString -> Either Syntax.Error Src.Module
+parse bs =
+  Parse.fromByteString Parse.Application bs
+
+outcomeToJson :: BSU.ByteString -> Outcome -> Json.Encode.Value
+outcomeToJson source outcome =
+  case outcome of
+    Success name file ->
+      Json.Encode.object
+        [ "type" ==> Json.Encode.chars "success",
+          "file" ==> Json.Encode.chars file,
+          "name" ==> Json.Encode.chars (ModuleName.toChars name)
+        ]
+    NoMain ->
+      reportToJson $ Ulm.Reporting.noMain
+    BadInput name err ->
+      reportToJson $
+        Reporting.Exit.Help.compilerReport
+          "/"
+          (Reporting.Error.Module name "/try" File.zeroTime source err)
+          []
+
+reportToJson :: Reporting.Exit.Help.Report -> Json.Encode.Value
+reportToJson =
+  Reporting.Exit.Help.reportToJson
+
+encodeJson :: Json.Encode.Value -> Wasm.JSString
+encodeJson value =
+  builderToJsString $ Json.Encode.encode value
+
+builderToJsString :: Data.ByteString.Builder.Builder -> Wasm.JSString
+builderToJsString builder =
+  let lazyStr :: Data.ByteString.Lazy.LazyByteString
+      lazyStr = Data.ByteString.Builder.toLazyByteString builder
+      str :: String
+      str = Data.ByteString.Lazy.UTF8.toString lazyStr
+   in Wasm.toJSString str
--- ulm-compiler-wasm/Ulm/ReadArtifacts.hs
+++ ulm-compiler-wasm/Ulm/ReadArtifacts.hs
@@ -1,5 +1,4 @@
-module Wasm.ReadArtifacts where
-
+module Ulm.ReadArtifacts where
 
 import AST.Optimized as Opt
 import Control.Monad (liftM, liftM2, liftM3)
@@ -7,17 +6,16 @@ import Data.Binary (Binary, decodeOrFail, get, put)
 import Data.Binary qualified as Binary
 import Data.ByteString.Lazy.UTF8 (ByteString, fromString)
 import Data.Map qualified as Map
-import Data.Maybe (mapMaybe, catMaybes)
+import Data.Maybe (catMaybes, mapMaybe, maybeToList)
 import Data.Set qualified as Set
 import Debug.Trace
 import Elm.Interface qualified as I
 import Elm.ModuleName qualified as ModuleName
 import Elm.Package qualified as Pkg
 import Elm.Version qualified as V
-import System.IO qualified as IO
 import GHC.Wasm.Prim
-import qualified System.Directory as Dir
-import Data.Maybe (maybeToList)
+import System.Directory qualified as Dir
+import System.IO qualified as IO
 
 data ArtifactsForWasm = ArtifactsForWasm
   { interfaces :: Map.Map ModuleName.Raw I.Interface,
@@ -26,13 +24,12 @@ data ArtifactsForWasm = ArtifactsForWasm
 
 getArtifactsForWasm :: IO ArtifactsForWasm
 getArtifactsForWasm =
-    do
-      artifacts <- getArtifacts
-      let 
-        interfaces :: [Map.Map ModuleName.Raw I.Interface]
+  do
+    artifacts <- getArtifacts
+    let interfaces :: [Map.Map ModuleName.Raw I.Interface]
         interfaces = map artifactToCompileInterface artifacts
-        in
-          pure $ ArtifactsForWasm 
+     in pure $
+          ArtifactsForWasm
             { interfaces = Map.unions interfaces,
               objects = foldr mergeGlobalObjectsGraphs Opt.empty artifacts
             }
@@ -83,33 +80,33 @@ getArtifacts :: IO [Artifacts]
 getArtifacts =
   do
     maybeArtifacts <-
-      mapM readArtifacts
-        [ "elm/core/1.0.5"
-        , "elm/html/1.0.0"
-        , "elm/browser/1.0.2"
-        , "elm/json/1.1.3"
-        , "elm/virtual-dom/1.0.3"
+      mapM
+        readArtifacts
+        [ "elm/core/1.0.5",
+          "elm/html/1.0.0",
+          "elm/browser/1.0.2",
+          "elm/json/1.1.3",
+          "elm/virtual-dom/1.0.3"
         ]
     return (catMaybes maybeArtifacts)
-  
 
 readArtifacts :: (Binary ArtifactCache) => FilePath -> IO (Maybe Artifacts)
 readArtifacts package =
-  let path = "/packages/" ++ package ++ "/artifacts.dat" in
-  do
-    cache <- readBinary path
-    return (fmap onlyArtifacts cache)
+  let path = "/packages/" ++ package ++ "/artifacts.dat"
+   in do
+        cache <- readBinary path
+        return (fmap onlyArtifacts cache)
 
 onlyArtifacts :: ArtifactCache -> Artifacts
 onlyArtifacts cache =
-    _artifacts cache
+  _artifacts cache
 
 readBinary :: (Binary a) => FilePath -> IO (Maybe a)
 readBinary path =
   -- / copied from builder/src/File.hs
   do
-    pathExists <- (traceShow ("does file exist? " ++ path ) (Dir.doesFileExist path))
-    
+    pathExists <- (traceShow ("does file exist? " ++ path) (Dir.doesFileExist path))
+
     if traceShowId pathExists
       then do
         result <- Binary.decodeFileOrFail path
@@ -118,18 +115,18 @@ readBinary path =
             return (Just a)
           Left (offset, message) ->
             do
-            --   IO.hPutStrLn IO.stderr $
-            --     unlines $
-            --       [ "+-------------------------------------------------------------------------------",
-            --         "|  Corrupt File: " ++ path,
-            --         "|   Byte Offset: " ++ show offset,
-            --         "|       Message: " ++ message,
-            --         "|",
-            --         "| Please report this to https://github.com/elm/compiler/issues",
-            --         "| Trying to continue anyway.",
-            --         "+-------------------------------------------------------------------------------"
-            --       ]
+              --   IO.hPutStrLn IO.stderr $
+              --     unlines $
+              --       [ "+-------------------------------------------------------------------------------",
+              --         "|  Corrupt File: " ++ path,
+              --         "|   Byte Offset: " ++ show offset,
+              --         "|       Message: " ++ message,
+              --         "|",
+              --         "| Please report this to https://github.com/elm/compiler/issues",
+              --         "| Trying to continue anyway.",
+              --         "+-------------------------------------------------------------------------------"
+              --       ]
               return Nothing
       else
         trace ("ERROR: file " ++ path ++ " does not exist") $
-           return Nothing
+          return Nothing
