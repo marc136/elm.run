eb368a8f5069d8220d5e3373c4135d37be38befb
Marc Walter <marc.walter@bsi-software.com>
2024-07-27 23:08:38 +0200

    Format code

--- ulm-editor/dev.mjs
+++ ulm-editor/dev.mjs
@@ -1,28 +1,26 @@
-import * as esbuild from 'esbuild'
-import { wasmLoader } from 'esbuild-plugin-wasm'
-
+import * as esbuild from "esbuild";
+import { wasmLoader } from "esbuild-plugin-wasm";
 
 let ctx = await esbuild.context({
-    entryPoints: [
-        'src/ulm-editor.ts',
-    ],
-    outdir: '../www/dist/editor',
-    format: 'esm',
-    outExtension: { '.js': '.mjs' },
-    bundle: true,
-    splitting: true,
-    sourcemap: 'external',
-    plugins: [
-        wasmLoader({
-            // (Default) Deferred mode copies the WASM binary to the output directory,
-            // and then `fetch()`s it at runtime. This is the default mode.
-            mode: 'deferred'
-        })
-    ]
-})
+  entryPoints: ["src/ulm-editor.ts"],
+  outdir: "../www/dist/editor",
+  format: "esm",
+  outExtension: { ".js": ".mjs" },
+  bundle: true,
+  splitting: true,
+  sourcemap: "external",
+  plugins: [
+    wasmLoader({
+      // (Default) Deferred mode copies the WASM binary to the output directory,
+      // and then `fetch()`s it at runtime. This is the default mode.
+      mode: "deferred",
+    }),
+  ],
+});
 
-await ctx.watch()
+await ctx.watch();
 const { host, port } = await ctx.serve({
-    servedir: '../www/dist',
-})
-console.log(`Serving on: http://${host}:${port}/`)
+  servedir: "../www/dist",
+  port: 1212,
+});
+console.log(`Serving on: http://${host}:${port}/`);
