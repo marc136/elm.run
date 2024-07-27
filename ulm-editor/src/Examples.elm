2b2070a0074e3086dd48fdec7b0628f362e8af86
Marc Walter <marc.walter@bsi-software.com>
2024-07-27 23:06:30 +0200

    Add code from https://elm-lang.org/try editor
    https://github.com/elm/elm-lang.org/blob/8193eb7552e26989d444167d6006eb50616ff554/editor/src/Data/Problem.elm

---
+++ ulm-editor/src/Examples.elm
@@ -0,0 +1,531 @@
+module Examples exposing
+    ( Example
+    , getCode
+    , list
+    )
+
+-- TODO generate this file from actual Elm source files
+--
+-- Copyright of these examples belongs to Evan Czaplicki
+-- See https://github.com/elm/elm-lang.org/tree/8193eb7552e26989d444167d6006eb50616ff554/examples
+
+
+type Example
+    = HelloWorld
+    | Buttons
+    | Clock
+    | Http
+    | Cards
+
+
+list : List ( String, Example )
+list =
+    [ ( "Hello World!", HelloWorld )
+    , ( "Buttons", Buttons )
+    , ( "Clock", Clock )
+    , ( "Http", Http )
+    , ( "Cards", Cards )
+    ]
+
+
+getCode : Example -> String
+getCode example =
+    case example of
+        HelloWorld ->
+            helloWorldCode
+
+        Buttons ->
+            buttonsCode
+
+        Clock ->
+            clockCode
+
+        Http ->
+            httpCode
+
+        Cards ->
+            cardsCode
+
+
+helloWorldCode : String
+helloWorldCode =
+    """import Html exposing (text)
+
+
+main =
+  text "Hello!"
+"""
+
+
+buttonsCode : String
+buttonsCode =
+    """module Main exposing (..)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = Int
+
+
+init : Model
+init =
+  0
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Increment
+  | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
+"""
+
+
+clockCode : String
+clockCode =
+    """-- Show an analog clock for your time zone.
+--
+-- For a simpler version, check out:
+--   https://elm-lang.org/examples/time
+--
+
+import Browser
+import Html exposing (Html)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+import Task
+import Time
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { zone : Time.Zone
+  , time : Time.Posix
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0)
+  , Cmd.batch
+      [ Task.perform AdjustTimeZone Time.here
+      , Task.perform Tick Time.now
+      ]
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
+
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime ->
+      ( { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  let
+    hour   = toFloat (Time.toHour   model.zone model.time)
+    minute = toFloat (Time.toMinute model.zone model.time)
+    second = toFloat (Time.toSecond model.zone model.time)
+  in
+  svg
+    [ viewBox "0 0 400 400"
+    , width "400"
+    , height "400"
+    ]
+    [ circle [ cx "200", cy "200", r "120", fill "#1293D8" ] []
+    , viewHand 6 60 (hour/12)
+    , viewHand 6 90 (minute/60)
+    , viewHand 3 90 (second/60)
+    ]
+
+
+viewHand : Int -> Float -> Float -> Svg msg
+viewHand width length turns =
+  let
+    t = 2 * pi * (turns - 0.25)
+    x = 200 + length * cos t
+    y = 200 + length * sin t
+  in
+  line
+    [ x1 "200"
+    , y1 "200"
+    , x2 (String.fromFloat x)
+    , y2 (String.fromFloat y)
+    , stroke "white"
+    , strokeWidth (String.fromInt width)
+    , strokeLinecap "round"
+    ]
+    []
+"""
+
+
+httpCode : String
+httpCode =
+    """-- Press a button to send a GET request for random quotes.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/json.html
+--
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, map4, field, int, string)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type Model
+  = Failure
+  | Loading
+  | Success Quote
+
+
+type alias Quote =
+  { quote : String
+  , source : String
+  , author : String
+  , year : Int
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  (Loading, getRandomQuote)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = MorePlease
+  | GotQuote (Result Http.Error Quote)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (Loading, getRandomQuote)
+
+    GotQuote result ->
+      case result of
+        Ok quote ->
+          (Success quote, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [ text "Random Quotes" ]
+    , viewQuote model
+    ]
+
+
+viewQuote : Model -> Html Msg
+viewQuote model =
+  case model of
+    Failure ->
+      div []
+        [ text "I could not load a random quote for some reason. "
+        , button [ onClick MorePlease ] [ text "Try Again!" ]
+        ]
+
+    Loading ->
+      text "Loading..."
+
+    Success quote ->
+      div []
+        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+        , blockquote [] [ text quote.quote ]
+        , p [ style "text-align" "right" ]
+            [ text "— "
+            , cite [] [ text quote.source ]
+            , text (" by " ++ quote.author ++ " (" ++ String.fromInt quote.year ++ ")")
+            ]
+        ]
+
+
+
+-- HTTP
+
+
+getRandomQuote : Cmd Msg
+getRandomQuote =
+  Http.get
+    { url = "https://elm-lang.org/api/random-quotes"
+    , expect = Http.expectJson GotQuote quoteDecoder
+    }
+
+
+quoteDecoder : Decoder Quote
+quoteDecoder =
+  map4 Quote
+    (field "quote" string)
+    (field "source" string)
+    (field "author" string)
+    (field "year" int)
+"""
+
+
+cardsCode : String
+cardsCode =
+    """-- Press a button to draw a random card.
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (..)
+import Random
+
+
+
+-- MAIN
+
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { card : Card
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Three
+  , Cmd.none
+  )
+
+
+type Card
+  = Ace
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Draw
+  | NewCard Card
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Draw ->
+      ( model
+      , Random.generate NewCard cardGenerator
+      )
+
+    NewCard newCard ->
+      ( Model newCard
+      , Cmd.none
+      )
+
+
+cardGenerator : Random.Generator Card
+cardGenerator =
+  Random.uniform Ace
+    [ Two
+    , Three
+    , Four
+    , Five
+    , Six
+    , Seven
+    , Eight
+    , Nine
+    , Ten
+    , Jack
+    , Queen
+    , King
+    ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Draw ] [ text "Draw" ]
+    , div [ style "font-size" "12em" ] [ text (viewCard model.card) ]
+    ]
+
+
+viewCard : Card -> String
+viewCard card =
+  case card of
+    Ace -> "🂡"
+    Two -> "🂢"
+    Three -> "🂣"
+    Four -> "🂤"
+    Five -> "🂥"
+    Six -> "🂦"
+    Seven -> "🂧"
+    Eight -> "🂨"
+    Nine -> "🂩"
+    Ten -> "🂪"
+    Jack -> "🂫"
+    Queen -> "🂭"
+    King -> "🂮"
+"""
