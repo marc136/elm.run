c01bec2f4631e6388a4df52a089ce9ef1ad09e84
Marc Walter <walter.marc@outlook.com>
2024-08-15 00:05:04 +0200

    Update GHC wasm backend

--- ulm-compiler-wasm/init.sh
+++ ulm-compiler-wasm/init.sh
@@ -1,1 +1,2 @@
-nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/1b4a14b3594cf0c0f73e82783b9c6714ca4a96c6/ghc-wasm-meta-master.tar.gz
+# nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/1b4a14b3594cf0c0f73e82783b9c6714ca4a96c6/ghc-wasm-meta-master.tar.g
+nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/1b4a14b3594cf0c0f73e82783b9c6714ca4a96c6/ghc-wasm-meta-master.tar.gz --extra-experimental-features nix-command --extra-experimental-features flakes
