e935ac453e9ec736a6240a0369fc8ba076337a36
Marc Walter <walter.marc@outlook.com>
2024-09-15 08:37:14 +0200

    Upgrade GHC

--- ulm-compiler-wasm/init.sh
+++ ulm-compiler-wasm/init.sh
@@ -1,2 +1,3 @@
-# nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/1b4a14b3594cf0c0f73e82783b9c6714ca4a96c6/ghc-wasm-meta-master.tar.g
-nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/1b4a14b3594cf0c0f73e82783b9c6714ca4a96c6/ghc-wasm-meta-master.tar.gz --extra-experimental-features nix-command --extra-experimental-features flakes
+COMMIT=a04cc1a2206d2030326e1d49be9c6a94ee4283a3
+nix shell https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/$COMMIT/ghc-wasm-meta-master.tar.gz --extra-experimental-features nix-command --extra-experimental-features flakes
+
